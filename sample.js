76e6dffc02f5a01fc73c0a39ce4b74d06a3ed2ba
Piyush-linux <piyushpatil98@protonmail.com>
2021-05-28 15:47:06 +0530

    qry params

--- sample.js
+++ sample.js
@@ -13,25 +13,32 @@ app.set("view engine", "pug")
 app.use(express.static(static))
 app.use(body_parser.urlencoded({ extended: true }))
 
-// mongo
-mongoose.connect("mongodb://localhost:27017/DBS", { useUnifiedTopology: true, useNewUrlParser: true });
-// rules -> collection -> field -> create
-const Schema = new mongoose.Schema({
+// MongoDB
+//- connection : rules -> collection -> field -> create
+mongoose.connect("mongodb://localhost:27017/itemDB", { useUnifiedTopology: true, useNewUrlParser: true });
+//- itm
+const itm_Schema = new mongoose.Schema({
     name: String,
 })
-const collection = mongoose.model("student", Schema)
-const field = new collection({
+const itm = mongoose.model("item", itm_Schema)
+const itm1 = new itm({
     name: "todo",
 })
-// field.save()
+//- lst
+const lst_Schema = new mongoose.Schema({
+    name: String,
+    list:[itm_Schema]
+})
+const lst = mongoose.model("lst", lst_Schema)
+// itm1.save()
 
 // get /
 app.get("/", (req, res) => {
     // read data
-    collection.find({}, (err, data) => {
+    itm.find({}, (err, data) => {
         // if empty inert default
         if (err) { console.log(err) } else {
-            res.render("index", { items: data })
+            res.render("index", { items: data || "hello" })
         }
     })
 })
@@ -39,30 +46,86 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
     // store on variable
     let i = req.body.item;
-    // // store on database
-    let itm = new collection({
+    // store on database
+    let itm1 = new itm({
         name: i
     })
-    itm.save().then(() => {
-        res.redirect("/")
+    itm1.save((err)=>{
+        if (err) {console.log(err)}
+            else {
+                console.log('Item inserted successfully O')
+            }
+            res.redirect("/")
     })
-    // res.redirect("/")
 })
 
 //  delete /
 app.post("/delete", (req, res) => {
-    // send value
+    // get value
     let del = req.body.checkbox
-    collection.deleteOne({ _id: del }, (err) => {
+    // search & delete
+    itm.deleteOne({ _id: del }, (err) => {
         if (err) { console.log(err) } else {
             console.log('Deleted successfull X')
         }
+    // redirect to get request
         res.redirect("/")
     })
 
 })
 
+// Query Page
+app.get("/:qry",(req,res)=>{
+    let page = req.params.qry;
+    lst.findOne({name:page},(err,data)=>{
+        if(!err){
+            if (!data) {
+                console.log('Doesnt exists')
+            }else{
+                console.log('exists')
+            }
+        }
+        // res.send(`<h1> Welcome to page  ${qry} </h1>`)
+    })
+    const lst1= new lst({
+        name: page,
+        list: itm1
+    })
+    lst1.save((err)=>{
+        if (!err) {
+            console.log('inseted page O')
+        }
+    res.send(`<h1> Welcome to page  ${page} </h1>`)
+    })
+})
+
+// Error Page
+// app.get("*")
+
 
 app.listen(3000, () => {
-    console.log('listening...')
+    console.log('listening http://localhost:3000')
 })
+
+/*
+
+- S.U.M.M.A.R.Y -
+
+# init
+express : server creation
+body-parser : form input get
+path : file resolve path
+mongoose : ODM betw node & MongoDB
+
+# Routes
+get /       = read data from DB & render on page
+post /      = get data from user & insert into DB
+post(del) / = get data from user & find from DB then delete
+get /:qry   = get params and res acc to qry
+
+# MongoDB
+
+itmDB
+    list[lst-todo]  )-(  todo[itm]
+
+*/
--- todo.md
+++ todo.md
@@ -1,8 +1,32 @@
 item
 html -> express -> mongodb -> express -> html
+
 # PJ
+
 __tech__ > express pug body-parser mongoose
 __context__
 - get data from db and render 
 - post data to db and set to get()
 - delete data as per there ID
+
+# Server
+mongodb://localhost:27017/DB
+mongodb => protocol , rules to acces data
+localhost:27017/DB => access DB
+
+# Database Deployment
+Server
+1. for http://
+	- Host Frontend + Backend
+	- heroku
+2. for mongodb://
+	- host DataBase
+	- atlas
+front + back [ DB , API ]
+__Mongodb Atlas__
+- DB host cloud provider : AWS , asure , google
+- security user with pasw
+- IP whitelist : aloow from anywhere
+- Cluster : connect with Monogo Shell 
+- cp mongo > terminal `mongo ...` then give pasw
+- app.js > connect(mongodb://username?pasw/DB) from cluter 
